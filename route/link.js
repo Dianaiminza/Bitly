c007ab107fc8adc21382b660f4d8173503be74cf
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-04-01 13:23:55 +0300

    Update link.js

--- route/link.js
+++ route/link.js
@@ -11,22 +11,45 @@ router.post("/new", async (req, res, next) => {
   // get url and full from input
   let { url, slug } = req.body;
   console.log("req.body: ", req.body);
+  if(!req.body.url) {
+    return res.status(400).send({"status": "error", "message": "A long URL is required"});
+   }
   
-  try {
-    
-    if (!slug) {
+   db.get("SELECT * FROM test WHERE url=?",
+   [req.body.url],
+   function (err, data) {
+   if(err) return res.status(500).json({
+     success:false,
+     message:err.message
+   })
+      if (!slug) {
       // generate slug if not provided by user
       const cryptoRandomString = require("crypto-random-string");
       slug = cryptoRandomString({ length: 10, type: "url-safe" }).toLowerCase();
     }
-    // insert everything in DB
+   console.log(data);
+  if(data){
+    res.json({
+          url: data.url,
+          slug:data.slug,
+          shorturl: `http://${req.headers.host}/${data.slug}`
+    })
+  }else {
+    try {
+     if (!slug) {
+      // generate slug if not provided by user
+      const cryptoRandomString = require("crypto-random-string");
+      slug = cryptoRandomString({ length: 10, type: "url-safe" }).toLowerCase();
+    }
     
-    db.exec(
+     // insert everything in DB
+    //check if the link is the same the slug remains the same
+    
+     db.exec(
       `INSERT INTO test(url, slug , clicks) VALUES('${url}', '${slug}', 0)`,
       (err) => {
         console.log("err: ", err);
         if (err) next(err);
-
         // response message has the short URL
         if (validUrl.isUri(url)){
         res.json({
@@ -37,30 +60,40 @@ router.post("/new", async (req, res, next) => {
       }else{
         res.json({message:"invalid url"})
       }
-      }
-    );
     
-  } catch (e) {
+      }
+  ); 
+    }
+  catch (e) {
     next(e);
   }
-});
-router.get("/:id", (req, res) => {
-  db.get(`SELECT * FROM test where slug='${req.params.id}'`, (err, data) => {
-    if (err) next(err);
-    // if (!data) res.redirect(`/?error=${req.params.id} not found!`);
-    // else res.redirect(data.url);
+ 
 
-    // add clicks
-    db.exec(
+  }
+   }) 
+   
+});
+
+router.get("/:id", (req, res) => {
+  db.get(`SELECT * FROM test  WHERE slug = ? AND url=?`,
+      [req.params.id,req.body.url],
+      function (err,data) {
+      if (err){
+        res.json({
+          success:false
+        })
+      }else
+       db.exec(
       `UPDATE test SET clicks = clicks + 1 WHERE slug='${req.params.id}'`,
       (err) => {
         if (err) console.error(err);
+        
         else res.redirect(data.url);
-       
       }
     );
-  });
+      });
 });
+
 router.get("/list", (req, res) => {
   db.all(`SELECT * FROM test`, (err, data) => {
     if (err) next(err);
@@ -71,3 +104,5 @@ router.get("/list", (req, res) => {
   });
 });
 module.exports=router; 
+
+//firebase implementation of this 
